0b4a319cf34101ecb6cef395e9de735c68c27c8b
M.S.Meenakshi <msmeenakshivoc@gmail.com>
2026-06-30 12:39:16 +0530

    Add files via upload
    Added SpringCoreMaven exercise 1.

---
+++ SpringCoreMavenExercise2/src/main/java/com/libraryy/LibraryManagementApplication.java
@@ -0,0 +1,20 @@
+package com.libraryy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                (BookService) context.getBean("bookService");
+
+        service.service();
+    }
+}
