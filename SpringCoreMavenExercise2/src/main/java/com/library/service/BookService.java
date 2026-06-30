d317ee53b18fecb64718e37af5948abac8d3ec71
M.S.Meenakshi <msmeenakshivoc@gmail.com>
2026-06-30 13:30:39 +0530

    Add files via upload
    Completed Exercise 2 - Dependency Injection

--- SpringCoreMavenExercise2/src/main/java/com/library/service/BookService.java
+++ SpringCoreMavenExercise2/src/main/java/com/library/service/BookService.java
@@ -1,8 +1,14 @@
 package com.library.service;
+import com.library.repository.BookRepository;
 
 public class BookService {
+	private BookRepository bookRepository;
+	public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 	public void service() {
         System.out.println("Book Service Created");
+        bookRepository.display();
     }
 
 }
